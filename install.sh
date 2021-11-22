1c50afe8cff51976a68085ca111713fc68317284
final <63569156+FinalLabs@users.noreply.github.com>
2021-11-22 01:22:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,15 +12,13 @@ clear
 $rootpartitionname
 $swappartitionname
 $bootpartitionname
+
 lsblk
 read -p "Enter number for root partition (ex. sdX1): " rootpartitionname
 read -p "Enter name for swap partition (ex. sdX2): " swappartitionname
 read -p "Enter name for boot partition (ex. sdX3): " bootpartitionname
-clear
 
-echo $rootpartitionname
-echo $swappartitionname
-echo $bootpartitionname
+clear
 
 mkfs.ext4 "/dev/$rootpartitionname"
 mkswap "/dev/$swappartitionname"
@@ -28,8 +26,8 @@ mkfs.fat -F 32 "/dev/$bootpartitionname"
 
 mount "/dev/$rootpartitionname" /mnt
 swapon "/dev/$swappartitionname"
-mkdir -p /mnt/boot
-mount "/dev/$bootpartitionname" /mnt/boot
+mkdir -p /mnt/boot/efi
+mount "/dev/$bootpartitionname" /mnt/boot/efi
 
 pacstrap /mnt base linux base-devel pulseaudio linux-headers linux-firmware nano grub efibootmgr wget
  
