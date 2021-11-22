1259cb19545f9de9ef14c0cbfb6137e39d41b797
final <63569156+FinalLabs@users.noreply.github.com>
2021-11-22 00:34:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,5 +41,35 @@ genfstab -U /mnt >> /mnt/etc/fstab
 clear
 read -p "press enter" enter
 
+touch /root/arch-install.sh
 
+$archchrootdir /root/arch-install.sh
+
+$scriptchroot "\$timename
+read -p 'Enter name for time (Region/City - ex. America/New_York):' timename
+ln -sf '/usr/share/zoneinfo/\$timename' /etc/localtime
+
+hwclock --systohc
+
+nano /etc/locale.gen
+locale-gen
+
+\$localename
+read -p 'Enter name for locale: ' localename
+
+echo 'LANG=\$localename' > /etc/locale.conf
+cat /etc/locale.conf
+
+\$hostname
+read -p 'Enter hostname:' hostname
+cat /etc/hostname
+
+echo 'Set password for root!'
+passwd
+
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+reboot"
+
+echo scriptchroot >> archchrootdir
 arch-chroot /mnt /root/chroot-install.sh
