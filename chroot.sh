85f42194750a8c0901fcf611fa4c26926b232519
final <63569156+FinalLabs@users.noreply.github.com>
2021-11-23 15:36:51 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 clear
 
+pacman -S zsh
+
 $timename
 read -p "Enter name for time (Region/City - ex. America/New_York): " timename
 ln -sf "/usr/share/zoneinfo/$timename" /etc/localtime
@@ -17,11 +19,16 @@ echo "LANG=$localename" > /etc/locale.conf
 cat /etc/locale.conf
 
 $hostname
-read -p "Enter hostname:" hostname
-echo "$hostname" > /etc/hostname
+read -p "Enter hostname: " hostname
+echo $hostname > /etc/hostname
 
 echo "Set password for root!"
 passwd
+
+$username
+read -p "Enter username: " username
+useradd -s /usr/bin/zsh -G wheel $username
+passwd $username
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 reboot
